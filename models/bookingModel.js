2b370f238d73fedea1bf9402302111b3b6ea1c11
Sebastian <jsga2505@gmail.com>
2024-05-09 18:45:24 -0500

    add booking controller

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
 
 const bookingSchema = mongoose.Schema({
-    nombre: String,
-    lugar: String,
-    precio: Number,
+    name: String,
+    place: String,
+    price: Number,
+    userId: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    experience:  [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "experience",
+        }
+    ]
 });
 
 const booking = mongoose.model("Booking", bookingSchema)
