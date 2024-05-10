436bf0cb480846318c032e3ecd894ac6bdc1f74b
Nelson <lvnelson.via@gmail.com>
2024-05-09 19:24:30 -0600

    Small fix on a model

--- models/experienceModel.js
+++ models/experienceModel.js
@@ -1,9 +1,9 @@
 import mongoose from "../config/mongoose.config.js";
 
 const experienceSchema = mongoose.Schema({
-name: String,
-place: String,
-price: Number,
+  name: String,
+  place: String,
+  price: Number,
 });
 
 const Experience = mongoose.model("Experience", experienceSchema);
