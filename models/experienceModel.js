bd101a87f4d3bd8c68c33612ab8e9f5879a8c2e3
Nelson <lvnelson.via@gmail.com>
2024-05-10 19:44:00 -0600

    Add requeriments to Experience Model

--- models/experienceModel.js
+++ models/experienceModel.js
@@ -1,9 +1,18 @@
 import mongoose from "../config/mongoose.config.js";
 
 const experienceSchema = mongoose.Schema({
-  name: String,
-  place: String,
-  price: Number,
+  name: {
+    type: String,
+    required: true
+  },
+  place: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  }
 });
 
 const Experience = mongoose.model("Experience", experienceSchema);
