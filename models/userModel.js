a064b778dba870d202a50066b4e642590a31c6be
Juanes <juanespachon1203@gmail.com>
2024-05-10 20:12:39 -0500

    Hash User Password

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,16 @@
 import mongoose from "../config/mongoose.config.js";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
   name: String,
   surname: String,
   email: String,
   password: String,
+});
+
+userSchema.pre('save', async function() {
+  this.password = await bcrypt.hash(this.password, 10);
+  return;
 });
 
 const User = mongoose.model("User", userSchema);
