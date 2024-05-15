11a274bf950537c7443e935016a6d464bfe9ea77
Juanes <juanespachon1203@gmail.com>
2024-05-15 16:31:21 -0500

    Install Cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import experienceRoutes from "./routes/experienceRoutes.js";
 
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 
