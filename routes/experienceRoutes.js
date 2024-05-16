a2f311ce1245096931df15f9f4b6760b69259394
Nelson <lvnelson.via@gmail.com>
2024-05-15 18:10:57 -0600

    Add private experience routes

--- routes/experienceRoutes.js
+++ routes/experienceRoutes.js
@@ -1,12 +1,14 @@
 import experienceController from "../controllers/experienceController.js";
 import express from "express"
+import { expressjwt } from "express-jwt";
+import "dotenv/config";
 
 const router = express.Router();
 
-router.get("/experiences", experienceController.listExperience);
-router.get("/experiences/:id", experienceController.findExperience);
-router.post("/experiences", experienceController.createExperience);
-router.patch("/experiences/:id", experienceController.editExperience)
-router.delete("experiences/:id", experienceController.deleteExperience);
+router.get("/experiences", experienceController.listExperience); // ver
+router.get("/experiences/:id", experienceController.findExperience); // encontrar
+router.post("/experiences", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}),experienceController.createExperience); // crear
+router.patch("/experiences/:id", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}),experienceController.editExperience); // editar
+router.delete("experiences/:id", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}),experienceController.deleteExperience); // eliminar
 
 export default router;
