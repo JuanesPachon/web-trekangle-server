30ab53b37fcd7025fc7027ae50c0985f036ebdfa
Sebastian <jsga2505@gmail.com>
2024-05-15 19:01:42 -0500

    Add private booking routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,14 @@
 import bookingController from "../controllers/bookingController.js";
 import express from "express";
+import { expressjwt } from "express-jwt";
+import "dotenv/config";
 
 const router = express.Router();
 
-router.get("/bookings", bookingController.listBooking);
-router.get("/bookings/:id", bookingController.findBooking);
-router.post("/bookings", bookingController.createBooking);
-router.patch("/bookings/:id", bookingController.editBooking);
-router.delete("/bookings/:id", bookingController.deleteBooking);
+router.get("/bookings", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}), bookingController.listBooking);
+router.get("/bookings/:id", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}), bookingController.findBooking);
+router.post("/bookings", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}), bookingController.createBooking);
+router.patch("/bookings/:id", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}), bookingController.editBooking);
+router.delete("/bookings/:id", expressjwt({algorithms: ["HS256"], secret: process.env.JWT_KEY}), bookingController.deleteBooking);
 
 export default router;
