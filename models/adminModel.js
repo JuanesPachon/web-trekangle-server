1f5e6b38bcf2eb6a651ecf2745838f497b886b4d
Juanes <juanespachon1203@gmail.com>
2024-05-16 17:25:43 -0500

    Create Admin Model

---
+++ models/adminModel.js
@@ -0,0 +1,18 @@
+import mongoose from "../config/mongoose.config.js";
+import bcrypt from "bcryptjs";
+
+const adminSchema = mongoose.Schema({
+  name: { type: String },
+  surname: { type: String },
+  email: { type: String },
+  password: { type: String },
+});
+
+adminSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+  return;
+});
+
+const Admin = mongoose.model("User", userSchema);
+
+export default Admin;
