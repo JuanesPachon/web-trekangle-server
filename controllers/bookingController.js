a0d688c244b334f5e13c37698ec86f3626be3289
Juanes <juanespachon1203@gmail.com>
2024-05-16 20:07:59 -0500

    Edit Admin/User validations in booking routes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,97 +1,127 @@
 import Booking from "../models/bookingModel.js";
 import User from "../models/userModel.js";
+import Admin from "../models/adminModel.js";
 
 async function listBooking(req, res) {
+  try {
+    const userId = req.auth.sub;
+    const { id: idUser } = await User.findById(userId);
+    const { id: idAdmin } = await Admin.findById(userId);
 
-    try {
-
-        const {id} = await User.findById(req.auth.sub);
-
-        const bookingList = await Booking.find({user: id}).populate("user").populate("experience");
-        res.json(bookingList)
-    } catch (error) {
-        res.status(500).json("The Server had an error");
+    if (idUser !== null) {
+      const bookingList = await Booking.find({ user: idUser })
+        .populate("user")
+        .populate("experience");
+      res.json(bookingList);
     }
+    if (idAdmin !== null) {
+      const bookingList = await Booking.find()
+        .populate("user")
+        .populate("experience");
+      res.json(bookingList);
+    }
+  } catch (error) {
+    res.status(500).json("The Server had an error");
+  }
 }
 
 async function findBooking(req, res) {
-    try {
+  try {
+    const { id } = await User.findById(req.auth.sub);
+    const foundBooking = await Booking.findById(req.params.id);
 
-        const {id} = await User.findById(req.auth.sub);
-        const foundBooking = await Booking.findById(req.params.id);
-
-        if(id === foundBooking.user[0].toString()) {
-            res.json(foundBooking);
-        } else {
-            res.json("This is not your booking, check again");
-        }
-        
-    } catch (error) {
-        res.status(500).json(error.message);
-        console.log(error)
+    if (id === foundBooking.user[0].toString()) {
+      res.json(foundBooking);
+    } else {
+      res.json("This is not your booking, check again");
     }
+  } catch (error) {
+    res.status(500).json(error.message);
+    console.log(error);
+  }
 }
 
 async function createBooking(req, res) {
-    try {
+  try {
+    const userId = req.auth.sub;
+    const { id: idUser } = await User.findById(userId);
 
-        const {id} = await User.findById(req.auth.sub);
-        const userId = req.body.user
-
-        if (id === userId) {
-        const newBooking = await Booking.create({
-            name: req.body.name,
-            place: req.body.place,
-            price: req.body.price,
-            user: req.body.user,
-            experience: req.body.experience
-        });
-        res.json(newBooking);
+    if (idUser !== null) {
+      const newBooking = await Booking.create({
+        name: req.body.name,
+        place: req.body.place,
+        price: req.body.price,
+        user: userId,
+        experience: req.body.experience,
+      });
+      res.json(newBooking);
     }
-    } catch (error) {
-        res.status(500).json("The server had an error");
-    }
+  } catch (error) {
+    res.status(500).json("The server had an error");
+  }
 }
 
 async function editBooking(req, res) {
-    try {
-        const {id} = await User.findById(req.auth.sub);
-        const booking = await User.findByid(req.params.id);
+  try {
+    const userId = req.auth.sub;
+    const { id: idUser } = await User.findById(userId);
+    const { id: idAdmin } = await Admin.findById(userId);
 
-        if (id === booking.user[0].toString()) {
-            const foundBooking = await User.findById(req,params.id);
+    if (idUser !== null) {
+      const foundBooking = await User.findById(req, params.id);
 
-            foundBooking.name = req.body.name || req.body.name;
-            foundBooking.place = req.body.place || req.body.place;
-            foundBooking.price = req.body.price || req.body.price;
+      if (foundBooking.user[0].toString() === idUser) {
+        foundBooking.name = req.body.name || req.body.name;
+        foundBooking.place = req.body.place || req.body.place;
+        foundBooking.price = req.body.price || req.body.price;
 
-            await foundBooking.save();
+        await foundBooking.save();
 
-            res.json(foundBooking);
-        }
-    } catch (error) {
-        res.status(500).json("The server had an error");
+        res.json(foundBooking);
+      }
     }
-} 
+    if (idAdmin !== null) {
+      const foundBooking = await User.findById(req, params.id);
+
+      foundBooking.name = req.body.name || req.body.name;
+      foundBooking.place = req.body.place || req.body.place;
+      foundBooking.price = req.body.price || req.body.price;
+
+      await foundBooking.save();
+
+      res.json(foundBooking);
+    }
+  } catch (error) {
+    res.status(500).json("The server had an error");
+  }
+}
 
 async function deleteBooking(req, res) {
-    try {
-        const {id} = await User.findById(req.auth.sub)
-        const booking = await Booking.findById(req.params.id);
+  try {
+    const userId = req.auth.sub;
+    const { id: idUser } = await User.findById(userId);
+    const { id: idAdmin } = await Admin.findById(userId);
 
-        if (id === booking.user[0].toString()) {
-            const deleteBooking = await Booking.findByIdAndDelete(req.params.id);
-            res.json("The booking was deleted");
-        }
-    } catch(error) {
-        res.status(500).json("The server had an error");
+    if (idUser !== null) {
+      const FoundBooking = await Booking.findById(req.params.id);
+      if (FoundBookingBooking.user[0].toString() === idUser) {
+        const deleteBooking = await Booking.findByIdAndDelete(req.params.id);
+        res.json("The booking was deleted");
+      }
     }
+    if (idAdmin !== null) {
+      const deleteBooking = await Booking.findByIdAndDelete(req.params.id);
+      res.json("The booking was deleted");
+    }
+  } catch (error) {
+    res.status(500).json("The server had an error");
+  }
 }
 
 export default {
-    listBooking,
-    findBooking,
-    createBooking,
-    editBooking,
-    deleteBooking,
-}
+  listBooking,
+  findBooking,
+  createBooking,
+  editBooking,
+  deleteBooking,
+};
