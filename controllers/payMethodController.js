3fcc14e97aa709facd9b2ca402615370bcc2381c
Sebastian <jsga2505@gmail.com>
2024-05-16 19:08:48 -0500

    add user validation in payMethod routes

--- controllers/payMethodController.js
+++ controllers/payMethodController.js
@@ -1,4 +1,6 @@
-import payMethod from "../models/payMethodModel";
+import payMethod from "../models/payMethodModel.js";
+import Admin from "../models/adminModel.js";
+import User from "../models/userModel.js";
 
 async function listpayMethod(req, res) {
 
@@ -14,10 +16,15 @@ async function listpayMethod(req, res) {
 async function findpayMethod(req, res) {
     try {
 
+        const {id: userId} = await User.findById(req.auth.sub);
+        const {id: adminId} = await Admin.findById(req.auth.sub);
         const foundpayMethod = await payMethod.findById(req.params.id);
 
+        if(userId === foundpayMethod.user[0].toString() || adminId === foundpayMethod.user[0].toString()) {
           res.json(foundpayMethod);
-        
+        } else {
+            res.json("This is not your pay method, check again");
+        }
     } catch (error) {
         res.status(500).json(error.message);
         console.log(error)
