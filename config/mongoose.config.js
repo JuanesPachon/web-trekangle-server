55445806a572a35e67fcb16cc3eff3f2366cca13
Juanes <juanespachon1203@gmail.com>
2024-06-08 12:41:22 -0500

    Edit MongoDB connection

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING); // connection string
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("Database connection error:", err));
 
 export default mongoose;
