821eac19e1606936a33277f06630c38ce64ad782
Juanes <juanespachon1203@gmail.com>
2024-06-16 23:15:41 -0500

    Edit User Validations

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -1,27 +1,28 @@
 import { check } from "express-validator";
+import User from "../models/userModel.js";
 
 const userValidations = [
   check("userName")
     .notEmpty()
     .withMessage("User Name's required")
-    .isLength({ min: 5, max: 10 })
-    .withMessage("Name's length has to be min 5 and max 10 characters"),
-
-  check("name")
-    .notEmpty()
-    .withMessage("Name's required")
-    .isLength({ min: 5, max: 10 })
-    .withMessage("Name's length is min 5 and max 10")
-    .isAlpha()
-    .withMessage("You cannot type numbers"),
+    .isLength({ min: 5, max: 20 })
+    .withMessage("Name's length has to be min 5 and max 20 characters")
+    .custom(async (value) => {
+      const existingUser = await User.findOne({ userName: value });
+      if (existingUser) {
+        throw new Error('Username is already in use');
+      }
+    }),
 
   check("name")
     .notEmpty()
     .withMessage("Name's required")
     .isLength({ min: 5 })
-    .withMessage("Name's length is min 5")
-    .isAlpha()
-    .withMessage("You cannot type numbers"),
+    .withMessage("Name's length is min 5characters"),
+
+  check("name")  
+    .matches(/^[a-zA-Z\s]+$/)
+    .withMessage("Name must be only letters"),
 
   check("email")
     .notEmpty()
@@ -32,8 +33,13 @@ const userValidations = [
   check("password")
     .notEmpty()
     .withMessage("Password's required")
-    .isLength({ min: 5, max: 10 })
-    .withMessage("Password's length has to be min 5 and max 10 characters"),
+    .isLength({ min: 5 })
+    .withMessage("Password's length has to be min 5 characters")
+    .matches(/\d/).withMessage('Password must contain at least one number')
+    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character'),
+  
 
 /*   check("profileImage").custom((value, { req }) => {
     const filetypes = /jpeg|jpg|png|/;
